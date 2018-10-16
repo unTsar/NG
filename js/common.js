ea1a81093f47586a5989e2258c41706a6b293bbd
unTsar <bycucaka@gmail.com>
2018-10-16 21:32:40 +0300

    555
    555

---
+++ js/common.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(document).snowfall({
+        flakeCount: 300,
+        image: "img/snow.png", 
+        minSize: 10, 
+        maxSize: 20,
+        round: true,
+        shadow: false,
+    });
+});
